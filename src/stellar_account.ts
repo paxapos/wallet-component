6137e6373211ba3ce2e15680acb56d0f0a3fe54f
tati2006 <101592692+tati2006@users.noreply.github.com>
2024-05-31 00:59:05 -0300

    Update stellar_account.ts

--- src/stellar_account.ts
+++ src/stellar_account.ts
@@ -8,9 +8,4 @@ export const stellarAccount2 = {
 };
 
 
-export const stellarAccount2 = {
-	pubKey: 'GAMMJJHWIQ3EUZ43DAQURVPTSTGXY35AA6UMSDISO6LS4QSKXMT4NQB2',
-	privKey: 'SD4XCUJFTSVYS375PMCKFWBM3B7RBKNI5ILGTQASABGX3U2BLI7GH4QP',
-};
-
 export const stellarNetwork = 'https://horizon-testnet.stellar.org';
