7a30f67f825486bca9af16fab72857574aa1cdf0
tati2006 <101592692+tati2006@users.noreply.github.com>
2024-06-07 17:03:21 -0300

    Update WalletQR.svelte

--- src/lib/WalletQR.svelte
+++ src/lib/WalletQR.svelte
@@ -15,6 +15,8 @@
 	export let className = 'qrcode';
 
 	let image = '';
+	let amount: number;
+	let recipient: string; 
 
 	function generateQrCode() {
 		QRcode.set({
@@ -44,4 +46,22 @@
 	});
 </script>
 
-<img src={image} alt={value} class={className} />
+<div  class=" flex items-center justify-center  "
+>
+	<div class=" bg-blue-200 p-6 rounded-lg shadow-lg w-200 ">
+		<h1 class="text-gray-800 text-3xl sixa-max"> QR</h1>
+		<div class="items-center justify-center mb-4">
+			<img src={image} alt={value} class={className} />
+		</div>
+		<div class=" items-center justify-center space-x-4">
+			<button
+				class="px-4 py-2 bg-red-400 text-white rounded-md hover:bg-red-600"
+				on:click={() => {
+					amount = 0;
+					recipient = '';
+				}}>Cancelar</button
+			>
+		</div>
+	</div>
+</div>
+
