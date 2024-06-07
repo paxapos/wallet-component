ac16726d761c47f064836f93ba48200885caac8f
tati2006 <101592692+tati2006@users.noreply.github.com>
2024-06-07 17:05:25 -0300

    Update InputPagar.svelte

--- src/lib/InputPagar.svelte
+++ src/lib/InputPagar.svelte
@@ -46,8 +46,8 @@
 <div
 	class="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50"
 >
-	<div class="bg-white p-6 rounded-lg shadow-lg w-80">
-		<h2 class="text-2xl font-bold mb-4">Enviar Transacción Stellar</h2>
+	<div class="bg-blue-200 p-6 rounded-lg shadow-lg w-80">
+		<h2 class="text-black text-2xl font-bold mb-4">Enviar Transacción Stellar</h2>
 		<div class="mb-4">
 			<label for="amount" class="block text-gray-700 mb-2">Monto</label>
 			<Input
@@ -59,8 +59,9 @@
 		</div>
 
 		<div class="mb-4">
-			<label for="amount" class="block text-gray-700 mb-2">Monto</label>
-			<Select
+			<label for="amount" class="block text-gray-700 mb-2">Activo</label>
+			<Select 
+				placeholder="Opciones"
 				class="mt-2"
 				items={monedasDisponibles}
 				bind:value={monedaSeleccionada}
@@ -78,18 +79,19 @@
 				class="w-full px-3 py-2 border"
 			/>
 		</div>
-		<div class="flex justify-end space-x-4">
+		<div class=" items-center justify-center space-x-4">
 			<button
-				class="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600"
+				class="px-4 py-2 bg-red-600 text-white rounded-md hover:bg-red-600"
 				on:click={() => {
 					amount = 0;
 					recipient = '';
 				}}>Cancelar</button
 			>
 			<button
-				class="px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-600"
+				class="px-4 py-2 bg-green-600 text-white rounded-md hover:bg-green-600"
 				on:click={submitForm}>Enviar</button
 			>
 		</div>
 	</div>
 </div>
+
