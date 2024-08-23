da638bdf7a9eb6e6dbdc1cbcfca2a0ed19da9fd6
Ortiz Franco <102056137+Franco0105@users.noreply.github.com>
2024-08-23 16:37:52 -0300

    Update Wallet.svelte

--- src/lib/Wallet.svelte
+++ src/lib/Wallet.svelte
@@ -162,7 +162,7 @@
 				/>
 			</svg>
 		</Button>
-		<Button class="bg-gray-300" outline color="light" on:click={pagar}>
+		<Button class="bg-gray-300" outline color="light" on:click={pagar} disabled={address.length != 56 && pasword.length != 56}>
 			Pagar
 			<svg
 				class="w-6 h-6 text-gray-800 dark:text-white"
@@ -172,7 +172,7 @@
 				height="24"
 				fill="none"
 				viewBox="0 0 24 24"
-			>
+>
 				<path
 					stroke="currentColor"
 					stroke-linecap="round"
@@ -180,13 +180,15 @@
 					stroke-width="2"
 					d="M8 17.345a4.76 4.76 0 0 0 2.558 1.618c2.274.589 4.512-.446 4.999-2.31.487-1.866-1.273-3.9-3.546-4.49-2.273-.59-4.034-2.623-3.547-4.488.486-1.865 2.724-2.899 4.998-2.31.982.236 1.87.793 2.538 1.592m-3.879 12.171V21m0-18v2.2"
 				/>
+				
 			</svg>
-		</Button>
+		</Button >
 		<Button
 			class="bg-gray-200"
 			outline
 			color="light"
 			on:click={WalletSaldo}
+			disabled={address.length != 56}
 		>
 			<svg
 				class="w-6 h-6 text-gray-800 dark:text-white"
