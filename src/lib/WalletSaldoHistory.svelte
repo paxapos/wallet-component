7a6f2f8efac6a1fa2ad1f3de66eb81552562f56f
tati2006 <101592692+tati2006@users.noreply.github.com>
2024-11-28 03:50:09 -0300

    Update WalletSaldoHistory.svelte

--- src/lib/WalletSaldoHistory.svelte
+++ src/lib/WalletSaldoHistory.svelte
@@ -1,79 +1,20 @@
 <script lang="ts">
 	import  listTransactions  from '../lib/Wallet.svelte';
+	listTransactions.forEach(transaccion => {
+    alert(`Tiene una nueva trasferencia de: ${transaccion}`);
+  });
 </script>
+
 
 	<div class="p-4 ">
 		<div class=" text-2xl font-bold mb-4 block px-4 py-2 font-medium text-center text-gray-700 rounded-t-lg dark:bg-gray-800 dark:text-white">
 			Actividad
 		</div>
-		<div class="divide-y divide-gray-100 dark:divide-gray-700">
-			<a href="#" class="flex px-4 py-3 hover:bg-gray-100 dark:hover:bg-gray-700">
-				<div class="flex-shrink-0">
-				  <div class="absolute flex items-center justify-center w-5 h-5 ms-6 -mt-5 bg-blue-600 border border-white rounded-full dark:border-gray-800">
-					<svg
-					class="w-6 h-6 text-gray-100 "
-					aria-hidden="true"
-					xmlns="http://www.w3.org/2000/svg"
-					width="24"
-					height="24"
-					fill="none"
-					viewBox="0 0 24 24"
-	>
-					<path
-						stroke="currentColor"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M8 17.345a4.76 4.76 0 0 0 2.558 1.618c2.274.589 4.512-.446 4.999-2.31.487-1.866-1.273-3.9-3.546-4.49-2.273-.59-4.034-2.623-3.547-4.488.486-1.865 2.724-2.899 4.998-2.31.982.236 1.87.793 2.538 1.592m-3.879 12.171V21m0-18v2.2"
-					/>
-					
-				</svg>
-				  </div>
-				</div>
-				<div class="w-full ps-3">
-					<div class="text-gray-500 text-sm mb-1.5 dark:text-gray-400">Se ha recibido una trasferencia de <span class="font-semibold text-gray-900 dark:text-white">USUARIO</span></div>
-					<div class="text-xs text-blue-600 dark:text-blue-500">Hace 10 minutos</div>
-				</div>
-			  </a>
-	
-		  <a href="#" class="flex px-4 py-3 hover:bg-gray-100 dark:hover:bg-gray-700">
-			<div class="flex-shrink-0">
-			  <div class="absolute flex items-center justify-center w-5 h-5 ms-6 -mt-5 bg-blue-600 border border-white rounded-full dark:border-gray-800">
-				<svg
-				class="w-6 h-6 text-gray-100 "
-				aria-hidden="true"
-				xmlns="http://www.w3.org/2000/svg"
-				width="24"
-				height="24"
-				fill="none"
-				viewBox="0 0 24 24"
->
-				<path
-					stroke="currentColor"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="2"
-					d="M8 17.345a4.76 4.76 0 0 0 2.558 1.618c2.274.589 4.512-.446 4.999-2.31.487-1.866-1.273-3.9-3.546-4.49-2.273-.59-4.034-2.623-3.547-4.488.486-1.865 2.724-2.899 4.998-2.31.982.236 1.87.793 2.538 1.592m-3.879 12.171V21m0-18v2.2"
-				/>
-				
-			</svg>
-			  </div>
-			</div>
-			<div class="w-full ps-3">
-				<div class="text-gray-500 text-sm mb-1.5 dark:text-gray-400">Se ha recibido una trasferencia de <span class="font-semibold text-gray-900 dark:text-white">USUARIO</span></div>
-				<div class="text-xs text-blue-600 dark:text-blue-500">Hace 35 minutos</div>
-			</div>
-		  </a>
-
-		</div>
-		<a href="#" class="block py-2 text-sm font-medium text-center text-gray-900 rounded-b-lg  hover:bg-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 dark:text-white">
-		  <div class="inline-flex items-center ">
-			<svg class="w-4 h-4 me-2 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 14">
-			  <path d="M10 0C4.612 0 0 5.336 0 7c0 1.742 3.546 7 10 7 6.454 0 10-5.258 10-7 0-1.664-4.612-7-10-7Zm0 10a3 3 0 1 1 0-6 3 3 0 0 1 0 6Z"/>
-			</svg>
-			  View all
-		  </div>
-		</a>
-	
+		<ul>
+			{#each listTransactions as transaccion}
+				<li>{transaccion}</li>
+			{/each}
+		</ul>
 	
 	</div>
+
